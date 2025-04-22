47e0b79ae74d3f23e0deed80d4043737500bc603
abhishekkrpandey17 <abhishekkrpandey17@gmail.com>
2025-04-22 23:56:08 +0530

    FEAT: Setup CMS part and  Created CMS form

---
+++ client/e-bindle/src/app/CMS/page.tsx
@@ -0,0 +1,13 @@
+
+import React from 'react'
+import CmsLogin from './cmsComponent/cmsLogin'
+
+const page = () => {
+  return (
+    <div>
+      <CmsLogin />
+    </div>
+  )
+}
+
+export default page
