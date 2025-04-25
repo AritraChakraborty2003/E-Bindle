f28ecb6298ca3e84d24f52aed38c375fd9d2121c
Aritra Chakraborty <aritra.chakraborty203@gmail.com>
2025-04-25 23:14:48 +0530

    Developed the controller

--- backend/index.js
+++ backend/index.js
@@ -38,6 +38,8 @@ app.use(
       process.env.NODE_ENV === "production"
         ? process.env.CORS_ORIGIN // e.g., "https://yourdomain.com"
         : "http://localhost:3000",
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   })
 );
